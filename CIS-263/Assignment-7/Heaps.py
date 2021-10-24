bb641d7e2f853e3531b26afdea15fdd27b63f791
juanbey <kejuanfb@gmail.com>
2021-10-24 15:45:31 -0400

    var changes

--- CIS-263/Assignment-7/Heaps.py
+++ CIS-263/Assignment-7/Heaps.py
@@ -1,32 +1,34 @@
-def __init__(self):
-    pass
 
-def minHeapify(self, A, i):
-    l = left(i)
-    r = right(i)
-    if l <= 12: 
+class MinHeap:
+    def __init__(self):
         pass
 
-def parent(self, i):
-    return (i // 2)
+    def minHeapify(self, A, i):
+        l = left(i)
+        r = right(i)
+        if l <= 12: 
+            pass
 
-def left(self, i):
-    return 2 * i
+    def parent(self, ind):
+        return (ind // 2)
 
-def right(self, i):
-    return (2 * i) + 1
+    def left(self, ind):
+        return (2 * ind)
 
-def buildMinheap(self, A):
-    pass
-def heapsort(self, A):
-    pass
-def heapmin(self, A):
-    pass
-def heapExtractmin(self):
-    pass
-def heapDecreasekey(self, i, key):
-    pass
-def minHeapinsert(self, key):
-    pass
-def buildMinheap(self, i):
-    pass
+    def right(self, ind):
+        return (2 * ind) + 1
+
+    def buildMinheap(self, A):
+        pass
+    def heapsort(self, A):
+        pass
+    def heapmin(self, A):
+        pass
+    def heapExtractmin(self):
+        pass
+    def heapDecreasekey(self, i, key):
+        pass
+    def minHeapinsert(self, key):
+        pass
+    def buildMinheap(self, i):
+        pass
