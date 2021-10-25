d9fdf82a5586ed04d3ae3b87b167e73ccbe0a1ab
juanbey <kejuanfb@gmail.com>
2021-10-24 23:50:31 -0400

    wifi fixed!

--- CIS-263/Assignment-7/Heaps.py
+++ CIS-263/Assignment-7/Heaps.py
@@ -1,24 +1,40 @@
 
 class MinHeap:
-    def __init__(self):
+    def __init__(self, max = 50):
+        self.max = max
+        self.size = 0
+        self.Heap = [0]
+        self.Heap = [0]
+
         pass
 
-    def minHeapify(self, A, i):
-        l = left(i)
-        r = right(i)
-        if l <= 12: 
+
+    def parent(self, index):
+        return (index // 2)
+
+    def left(self, index):
+        return (2 * index)
+
+    def right(self, index):
+        return (2 * index) + 1
+
+
+
+
+    def minHeapify(self, index):
+        self.l = left(index)
+        self.r = right(index)
+        if self.l <= len(array) and array[self.l] < array[index]: 
+            smallest = self.l
+        else:
+            smallest = index
             pass
 
-    def parent(self, ind):
-        return (ind // 2)
 
-    def left(self, ind):
-        return (2 * ind)
+    def buildMinheap(self, array):
+        size = len(array) // 2
+        for i in range(size, 0 ,-1):
 
-    def right(self, ind):
-        return (2 * ind) + 1
-
-    def buildMinheap(self, A):
         pass
     def heapsort(self, A):
         pass
@@ -29,6 +45,4 @@ class MinHeap:
     def heapDecreasekey(self, i, key):
         pass
     def minHeapinsert(self, key):
-        pass
-    def buildMinheap(self, i):
         pass
