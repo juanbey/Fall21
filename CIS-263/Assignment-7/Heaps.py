10adda1143804bf4556fc04680802783e74b6951
juanbey <kejuanfb@gmail.com>
2021-10-26 01:56:47 -0400

    new methods

--- CIS-263/Assignment-7/Heaps.py
+++ CIS-263/Assignment-7/Heaps.py
@@ -13,16 +13,28 @@ class MinHeap:
 
     def right(self, ind):
         return (2 * ind) + 1
-    def minHeapify(self, index):
-        self.l = self.left(index)
-        self.r = self.right(index)
-        if self.l <= self.heap[self.size] and self.left(index) < self.heap[index]: 
-            smallest = self.l
-        else:
-            smallest = index
-            pass
+    def decreasekey(self, index, value):
+        if value < self.heap[index]:      
+            raise Exception(f"{value} is larger than {self.heap[index]}")
+        self.heap[index] = value
+        while index > 1 and self.heap[self.parent(index)]:
+            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]
+            index = self.parent(index)  
+    def heapinsert(self, value):
+        if value < self.min:
+            print(f"Value must be less than {self.min} entered: {value}")
+            return
+        self.size += 1
+        current = self.size
+        
+        self.heap[current] = value
+        self.decreasekey(current, value)
 
 
+
+
+
+#TODO--------------------------------------------------
     def buildMinheap(self, array):
         size = len(array) // 2
         self.heap = [0] + array[:]
@@ -33,23 +45,13 @@ class MinHeap:
         pass
     def heapmin(self, A):
         pass
-    def heapExtractmin(self):
+    def extractmin(self):
         pass
-    def heapDecreasekey(self, index, key):
-        if key < self.heap[index]:      
-            raise Exception(f"{key} is larger than {self.heap[index]}")
-        self.heap[index] = key
-        while index > 1 and self.heap[self.parent(index)]
-        pass
-    def minHeapinsert(self, value):
-        if value < self.min:
-            print(f"Value must be less than {self.min} entered: {value}")
-            return
-        self.size += 1
-        current = self.size
-        
-        self.heap[current]= value
-
-        while self.heap[current] < self.heap[self.parent[current]]:
+    def minHeapify(self, index):
+        self.l = self.left(index)
+        self.r = self.right(index)
+        if self.l <= self.heap[self.size] and self.left(index) < self.heap[index]: 
+            smallest = self.l
+        else:
+            smallest = index
             pass
-        pass
