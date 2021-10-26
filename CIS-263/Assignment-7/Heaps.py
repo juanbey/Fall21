1c6afe5efca31a63f5d92b5e1da1de878d08e0e1
juanbey <kejuanfb@gmail.com>
2021-10-26 02:34:27 -0400

    oop probs

--- CIS-263/Assignment-7/Heaps.py
+++ CIS-263/Assignment-7/Heaps.py
@@ -1,9 +1,8 @@
 class MinHeap:
-    def __init__(self, min = 0):
+    def __init__(self, min = 0, array = None):
         self.min = min
         self.size = 0
         self.heap = []
-        
 
     def parent(self, ind):
         return (ind // 2)
@@ -13,6 +12,20 @@ class MinHeap:
 
     def right(self, ind):
         return (2 * ind) + 1
+
+    def minHeapify(self, index, array):
+        self.l = self.left(index)
+        self.r = self.right(index)
+        if self.l <= len(self.heap) and self.left(index) < self.heap[index]: 
+            smallest = self.l
+        else:
+            smallest = index
+        if self.r <= len(self.heap) and self.heap[self.r] < self.heap[smallest]:
+            smallest = self.r
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self.minHeapify(smallest)
+
     def decreasekey(self, index, value):
         if value < self.heap[index]:      
             raise Exception(f"{value} is larger than {self.heap[index]}")
@@ -20,6 +33,7 @@ class MinHeap:
         while index > 1 and self.heap[self.parent(index)]:
             self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]
             index = self.parent(index)  
+
     def heapinsert(self, value):
         if value < self.min:
             print(f"Value must be less than {self.min} entered: {value}")
@@ -29,29 +43,21 @@ class MinHeap:
         
         self.heap[current] = value
         self.decreasekey(current, value)
-
-
-
-
+    
+    
+    def heapmin(self):
+        return self.heap[1]
 
 #TODO--------------------------------------------------
     def buildMinheap(self, array):
         size = len(array) // 2
         self.heap = [0] + array[:]
-        for i in range(size, 0 ,-1):
-            pass
+        for i in range(size, -1,-1):
+            self.minHeapify(i, array)
         pass
     def heapsort(self, A):
         pass
-    def heapmin(self, A):
-        pass
+
     def extractmin(self):
         pass
-    def minHeapify(self, index):
-        self.l = self.left(index)
-        self.r = self.right(index)
-        if self.l <= self.heap[self.size] and self.left(index) < self.heap[index]: 
-            smallest = self.l
-        else:
-            smallest = index
-            pass
+   
